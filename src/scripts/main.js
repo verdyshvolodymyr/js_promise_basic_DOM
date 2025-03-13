35f13ecc06906c38aeb3235fc68ee2404f39184a
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-03-13 23:08:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+logo.addEventListener('click', hendler);
+
+const messageSuc = 'Promise was resolved!';
+const messageErr = 'Promise was rejected!';
+
+function first() {
+  const div = document.createElement('div');
+  div.className = 'message'
+  div.textContent = messageSuc;
+
+  body.append(div)
+}
+
+function second() {
+  const div = document.createElement('div');
+  div.className = 'message error-message'
+  div.textContent = messageErr;
+
+  body.append(div)
+}
+
+function hendler() {
+  return new Promise((resolve, reject) => {
+
+    resolve(first());
+  })
+}
+
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(second()), 3000);
+});
